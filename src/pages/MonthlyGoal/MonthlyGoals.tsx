a5ccb61f751d4577c7f42ccd3cfa39209219cedf
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-07-30 15:03:02 +0900

    過去の達成率のグラフを作成

--- src/pages/MonthlyGoal/MonthlyGoals.tsx
+++ src/pages/MonthlyGoal/MonthlyGoals.tsx
@@ -1,14 +1,34 @@
-import Header from "@/components/header"
-import { ChartRadialShape } from "./chart";
+import Header from "@/components/header";
+import { ChartRadialShape } from "./piChart";
+import { useEffect, useState } from "react";
+import { api } from "@/lib/axios";
+import type { Monthly_goals } from "@/types";
+import { AchievementChart } from "./linearChart";
 
 export default function MonthlyGoals() {
+  const [goals, setGoals] = useState<Monthly_goals[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const fetchGoals = async () => {
+      try {
+        const response = await api.get("/monthly_goals");
+        setGoals(response.data);
+      } catch {
+        setError("取得に失敗しました");
+      }
+    };
+
+    fetchGoals();
+  }, []);
   return (
     <>
-      <Header/>
+      <Header />
 
-      <div className="p-5">
-          <ChartRadialShape/>
+      <div className="p-15">
+        {error && <p>{error}</p>}
+        <ChartRadialShape goals={goals} />
+        <AchievementChart goals={goals}/>
       </div>
     </>
   );
---
+++ src/pages/MonthlyGoal/linearChart.tsx
@@ -0,0 +1,83 @@
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
+import type { Monthly_goals } from "@/types";
+
+type Props = {
+  goals: Monthly_goals[];
+};
+
+const chartConfig = {
+  achievement: {
+    label: "達成率",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig;
+
+export function AchievementChart({ goals }: Props) {
+  //月目標を古い順にしてグラフ用に配列を作成
+  const chartData = [...goals].reverse().map((goal) => ({
+    //JavaScriptのDateオブジェクトにし, 表示用の文字列に変更
+    month: new Date(goal.goal_date).toLocaleDateString("ja-JP", {
+      year: "2-digit",
+      month: "numeric",
+    }),
+    //達成率
+    achievement: goal.achievement_rate,
+  }));
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>達成率の推移</CardTitle>
+        <CardDescription>これまでの月目標達成率</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+            />
+
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dot" />}
+            />
+
+            <Area
+              type="linear"
+              dataKey="achievement"
+              stroke="var(--color-achievement)"
+              fill="var(--color-achievement)"
+              fillOpacity={0.3}
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
