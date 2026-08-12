0d53e19a30ed51855235f447eff559b3e45e9e9d
Atsushi-iwaguchi <i.oneroom.bn@gmail.com>
2026-08-12 12:25:30 +0900

    本番環境用にaxios.tsを変更

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 
 //共通して使うAPIのURLを設定
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-    //localstorageからトークンを取得
+  //localstorageからトークンを取得
   const token = localStorage.getItem("token");
 
   if (token) {
